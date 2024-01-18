a2cb49f7e6127a839c1af09d13ad84dcb3f15b3f
Jason Cassidy <47318351+jcassidyav@users.noreply.github.com>
2024-01-18 15:34:02 +0000

    cleanup

--- angular/app/main.ts
+++ angular/app/main.ts
@@ -1,6 +1,8 @@
 import { platformNativeScript, registerElement, runNativeScriptAngularApp } from '@nativescript/angular';
 
 import { AppModule } from './app/app.module';
+import { enableProdMode } from "@angular/core";
+enableProdMode();
 registerElement('HTMLLabel', () => require('@nativescript-community/ui-label').Label);
 runNativeScriptAngularApp({
   appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
